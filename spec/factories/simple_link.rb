ca4f04cc1f0bc0d5e18f12d1dcd19158255907d4
jalrax <trymental@gmail.com>
2024-01-18 22:36:46 +0300

    Added a few specs

--- spec/factories/simple_link.rb
+++ spec/factories/simple_link.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :simple_link do
     sequence(:url) { |n| "https://github.com/jalrax/simple-ruby-url-shortener-#{n}" }
-    short_url { nil }
+    short_url { SecureRandom.uuid[0..5] }
     click_count { 0 }
   end
 end
--- spec/requests/simple_link_spec.rb
+++ spec/requests/simple_link_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe SimpleLinksController do
+  describe '#get_url' do
+    let(:simple_link) { create :simple_link }
+    subject { get "/urls/#{simple_link.short_url}" }
+    before { subject }
 
+    it 'should return a success responce' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should return a long url' do
+      long_url = simple_link.url
+
+      expect(response.body).to match long_url
+    end
+
+    it 'should increase counter by 1' do
+      new_link = create :simple_link
+      counter = new_link.click_count
+
+      get "/urls/#{new_link.short_url}"
+      new_link.reload
+
+      expect(new_link.click_count).to eq counter + 1
+    end
+  end
+
+  describe '#get_url_stats' do
+    let(:simple_link) { create :simple_link }
+    subject { get "/urls/#{simple_link.short_url}/stats" }
+    before { subject }
+
+    it 'should return simple_link click count' do
+      expect(response.body).to eq '0'
+    end
+  end
 end
